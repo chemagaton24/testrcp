11c6a6e0b33887c01e513a895ec309f5258b36cd
chemagaton24 <chembeeragaton@gmail.com>
2023-11-23 21:01:44 +0800

    asd

--- pages/api/test1/index.js
+++ pages/api/test1/index.js
@@ -6,34 +6,57 @@ const handler = async (req, res) => {
     formData.append("u", "61572bb8acf7b8704903af7b8");
     formData.append("id", "036f88a1e3");
 
-    axios.post(
-      "https://realclearpolitics.us7.list-manage.com/subscribe/post",
-      formData,
+    const response = await fetch(
+      "https://realclearpolitics.us7.list-manage.com/subscribe/post?MERGE0=chembeer.agaton@codeandtheory.com&u=61572bb8acf7b8704903af7b8&id=036f88a1e3",
       {
-        headers: { "Content-Type": "multipart/form-data" },
-        transformRequest: (formData) => formData,
+        method: "GET",
       }
     );
-
-    res.status(201).send({ success: "strue" });
-
-    // const formData = new FormData();
-    // formData.append("MERGE0", "chembeer.agaton@codeandtheory.com");
-    // formData.append("u", "61572bb8acf7b8704903af7b8");
-    // formData.append("id", "036f88a1e3");
-
-    // const response = await fetch(
-    //   "https://realclearpolitics.us7.list-manage.com/subscribe/post",
-    //   {
-    //     method: "POST",
-    //     body: formData,
-    //   }
-    // );
-    // const dataHTML = await response.text();
-    // res.status(201).send(dataHTML);
+    const dataHTML = await response.text();
+    res.status(201).send(dataHTML);
   } catch (err) {
     res.status(500).send({ success: false, message: err.message });
   }
+
+  // const response = await fetch(
+  //   "https://realclearpolitics.us7.list-manage.com/subscribe/post",
+  //   {
+  //     method: "POST",
+  //     body: formData,
+  //   }
+  // );
+
+  // await axios.get("http://minhaserieapi.herokuapp.com/search/vikings");
+
+  // await axios.post(
+  //   "https://realclearpolitics.us7.list-manage.com/subscribe/post",
+  //   formData,
+  //   {
+  //     headers: {
+  //       "Content-Type": "multipart/form-data",
+  //     },
+  //   }
+  // );
+
+  // res.status(201).send({ success: "strue" });
+
+  // const formData = new FormData();
+  // formData.append("MERGE0", "chembeer.agaton@codeandtheory.com");
+  // formData.append("u", "61572bb8acf7b8704903af7b8");
+  // formData.append("id", "036f88a1e3");
+
+  //   const response = await fetch(
+  //     "https://realclearpolitics.us7.list-manage.com/subscribe/post",
+  //     {
+  //       method: "POST",
+  //       body: formData,
+  //     }
+  //   );
+  //   const dataHTML = await response.text();
+  //   res.status(201).send(dataHTML);
+  // } catch (err) {
+  //   res.status(500).send({ success: false, message: err.message });
+  // }
 };
 
 export default handler;

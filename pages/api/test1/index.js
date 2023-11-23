87c135e2aeddde3ae8f229295fbc929e40e8ab15
Chembeer Agaton <chembeeragaton@gmail.com>
2023-11-23 15:08:46 +0800

    asd

--- pages/api/test1/index.js
+++ pages/api/test1/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 const handler = async (req, res) => {
   try {
     const formData = new FormData();
@@ -5,15 +6,31 @@ const handler = async (req, res) => {
     formData.append("u", "61572bb8acf7b8704903af7b8");
     formData.append("id", "036f88a1e3");
 
-    const response = await fetch(
+    axios.post(
       "https://realclearpolitics.us7.list-manage.com/subscribe/post",
+      formData,
       {
-        method: "POST",
-        body: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+        transformRequest: (formData) => formData,
       }
     );
-    const dataHTML = await response.text();
-    res.status(201).send(dataHTML);
+
+    res.status(201).send({ success: "strue" });
+
+    // const formData = new FormData();
+    // formData.append("MERGE0", "chembeer.agaton@codeandtheory.com");
+    // formData.append("u", "61572bb8acf7b8704903af7b8");
+    // formData.append("id", "036f88a1e3");
+
+    // const response = await fetch(
+    //   "https://realclearpolitics.us7.list-manage.com/subscribe/post",
+    //   {
+    //     method: "POST",
+    //     body: formData,
+    //   }
+    // );
+    // const dataHTML = await response.text();
+    // res.status(201).send(dataHTML);
   } catch (err) {
     res.status(500).send({ success: false, message: err.message });
   }

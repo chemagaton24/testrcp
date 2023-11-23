de226aff49338e3a04d3101768fa7b3beea213c0
Chembeer Agaton <chembeeragaton@gmail.com>
2023-11-23 14:46:15 +0800

    asd

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,18 +1,22 @@
 const handler = async (req, res) => {
-  const formData = new FormData();
-  formData.append("MERGE0", "chembeer.agaton@codeandtheory.com");
-  formData.append("u", "61572bb8acf7b8704903af7b8");
-  formData.append("id", "036f88a1e3");
+  try {
+    const formData = new FormData();
+    formData.append("MERGE0", "chembeer.agaton@codeandtheory.com");
+    formData.append("u", "61572bb8acf7b8704903af7b8");
+    formData.append("id", "036f88a1e3");
 
-  const response = await fetch(
-    "https://realclearpolitics.us7.list-manage.com/subscribe/post",
-    {
-      method: "POST",
-      body: formData,
-    }
-  );
-  const dataHTML = await response.text();
-  res.status(201).send({ success: true, message: "success!", dataHTML });
+    const response = await fetch(
+      "https://realclearpolitics.us7.list-manage.com/subscribe/post",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    const dataHTML = await response.text();
+    res.status(201).send({ success: true, message: "success!", dataHTML });
+  } catch (err) {
+    res.status(500).send({ success: false, message: err.message });
+  }
 };
 
 export default handler;

9322b08905e924ec02b209b0c5e42a31b220b839
Chembeer Agaton <chembeeragaton@gmail.com>
2023-11-23 14:43:27 +0800

    asd

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,18 @@
 const handler = async (req, res) => {
-  res.status(201).send({ success: true, message: "success!" });
+  const formData = new FormData();
+  formData.append("MERGE0", "chembeer.agaton@codeandtheory.com");
+  formData.append("u", "61572bb8acf7b8704903af7b8");
+  formData.append("id", "036f88a1e3");
+
+  const response = await fetch(
+    "https://realclearpolitics.us7.list-manage.com/subscribe/post",
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+  const dataHTML = await response.text();
+  res.status(201).send({ success: true, message: "success!", dataHTML });
 };
 
 export default handler;
